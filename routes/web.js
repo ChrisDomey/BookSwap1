2769cb42d86be99c564ba26ba8c701e350f8b882
Krishna <kr1shna@outlook.com>
2018-03-12 16:37:23 -0700

    added routes for register

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-// const User = require('../models/User')
+const expressValidator = require('express-validator')
+const User = require('../models/User')
 
 /* GET home page. */
 router.get('/', async (req, res) => {
@@ -8,6 +9,15 @@ router.get('/', async (req, res) => {
     // const username = user.get('username')
 
     res.render('index', { title: 'Voyager' })
+})
+
+router.get('/register',function(req,res){
+    res.render('register',{title:'Registration'})
+})
+
+router.post('/register',function(req,res){
+    req.checkBody('username',"Username can not be empty").notEmpty();
+    res.render('register',{title:"REgistration failed"})
 })
 
 module.exports = router
