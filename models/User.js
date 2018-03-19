c4cadffa34a7d90ca8ee951b8d651d6cc6d8fa38
krishna <kr1shna@outlook,com>
2018-03-19 12:22:24 -0400

    Added create method for user registration

--- models/User.js
+++ models/User.js
@@ -1,7 +1,16 @@
 const bookshelf = require('../config/bookshelf')
+const bcrypt = require('bcrypt');
 
 const User = bookshelf.Model.extend({
-    tableName: 'user'
-})
+    tableName: 'user',
+    verifyPassword: function(password){
+        return bcrypt.compare(password, this.get('password'),function(err, res) {
+        return(res)
+    })}},
+    {
+        create: function(data){
+            return this.forge(data).save()
+        }
+    })
 
 module.exports = User
