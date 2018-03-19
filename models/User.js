48c9cc688d29443c9fd328a3f85f39c75862bb55
krishna <kr1shna@outlook,com>
2018-03-19 13:19:29 -0400

    corrected verifyPassword function

--- models/User.js
+++ models/User.js
@@ -3,14 +3,18 @@ const bcrypt = require('bcrypt');
 
 const User = bookshelf.Model.extend({
     tableName: 'user',
-    verifyPassword: function(password){
-        return bcrypt.compare(password, this.get('password'),function(err, res) {
-        return(res)
-    })}},
+    verifyPassword: function (password, fn) {
+        bcrypt.compare(password, this.get('password'), function (err, result) {
+            fn(result)
+        })
+
+    }
+},
     {
-        create: function(data){
+        create: function (data) {
             return this.forge(data).save()
         }
     })
+
 
 module.exports = User
