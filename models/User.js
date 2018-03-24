6c53773394ecb5d3df950ed2fb2366c93a3817ca
Krishna Chaitanya Nelloore <kr1shna@outlook.com>
2018-03-24 05:50:55 -0700

    made mywishlist and mybooks functions non-static

--- models/User.js
+++ models/User.js
@@ -7,20 +7,18 @@ const User = bookshelf.Model.extend({
         bcrypt.compare(password, this.get('password'), function (err, result) {
             fn(result)
         })
+    },
+    myBooks: function () {
+        return this.hasMany('UserBook', 'username')
+    },
+    myWishlist: function () {
+        return this.hasMany('UserWishlist', 'username')
     }
 },
     {
         create: function (data) {
             return this.forge(data).save()
         },
-
-        myBooks:function(){
-            return this.hasMany('UserBook','username')
-        },
-        myWishlist: function(){
-            return this.hasMany('UserWishlist','username')
-        }
-
     })
 
 
