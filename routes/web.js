378ce0009271c9ec4b232d7f3ddccc37f77ac9eb
Manmitha Krishna <manmitha.ck@gmail.com>
2018-04-08 12:50:42 -0400

    results view page added

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,4 @@
-const express = require('express')
+ const express = require('express')
 const router = express.Router()
 const expressValidator = require('express-validator')
 const User = require('../models/User')
