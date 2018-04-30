33a97071a6653204bd41512d899af33e63b453c7
krishna <kr1shna@outlook.com>
2018-04-29 22:40:19 -0400

    error handler fixed

--- app.js
+++ app.js
@@ -106,12 +106,13 @@ passport.use(new LocalStrategy(
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
     const err = new Error('Not Found')
+    console.log("akjsndkjnajskdnkansdkansdkjn")
     err.status = 404
     next(err)
 })
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message
     res.locals.error = req.app.get('env') === 'development' ? err : {}
